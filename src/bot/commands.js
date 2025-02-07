e447eb94c514dd2fdfafe8436c14bc7ef7c17a48
ThawZin999 <egthawzin@gmail.com>
2025-02-07 19:22:03 +0630

    kotoba forward test1

--- src/bot/commands.js
+++ src/bot/commands.js
@@ -1,14 +1,5 @@
 import { getMainMenu, getInlineKeyboard } from "./menus.js";
 import { Markup } from "telegraf";
-import { n5Kotoba1, n5Kotoba2 } from "./kotoba.js";
-
-function escapeMarkdownV2(text) {
-  return text.replace(/([_*[\]()~`>#+-={}.!])/g, "\\$1");
-}
-
-function removePipes(text) {
-  return text.replace(/\|\|/g, "");
-}
 
 export const setupCommands = (bot) => {
   bot.start((ctx) => {
@@ -106,62 +97,66 @@ export const setupCommands = (bot) => {
 
   bot.hears("N5 Kotoba", (ctx) => {
     ctx.reply(
-      "ဉပမာ-Unit 1 Kotobaကျက်ရန်အတွက်N5 1ကိုရွေးချယ်ပါ။ Quizဖြေရန်အတွက် N5 1 Quizကိုထပ်ခါထပ်ခါနှိပ်သွားပါ။",
+      "ဉပမာ-Unit 1 Kotobaကျက်ရန်အတွက်N5 1ကိုရွေးချယ်ပါ။ ",
       Markup.keyboard([
-        ["N5 1", "N5 1 Quiz"],
-        ["N5 2", "N5 2 Quiz"],
-        ["N5 3", "N5 3 Quiz"],
-        ["N5 4", "N5 4 Quiz"],
-        ["N5 5", "N5 5 Quiz"],
-        ["N5 6", "N5 6 Quiz"],
-        ["N5 7", "N5 7 Quiz"],
-        ["N5 8", "N5 8 Quiz"],
-        ["N5 9", "N5 9 Quiz"],
-        ["N5 10", "N5 10 Quiz"],
-        ["N5 11", "N5 11 Quiz"],
-        ["N5 12", "N5 12 Quiz"],
-        ["N5 13", "N5 13 Quiz"],
-        ["N5 14", "N5 14 Quiz"],
-        ["N5 15", "N5 15 Quiz"],
-        ["N5 16", "N5 16 Quiz"],
-        ["N5 17", "N5 17 Quiz"],
-        ["N5 18", "N5 18 Quiz"],
-        ["N5 19", "N5 19 Quiz"],
-        ["N5 20", "N5 20 Quiz"],
-        ["N5 21", "N5 21 Quiz"],
-        ["N5 22", "N5 22 Quiz"],
-        ["N5 23", "N5 23 Quiz"],
-        ["N5 24", "N5 24 Quiz"],
-        ["N5 25", "N5 25 Quiz"],
-        ["Back"],
+        ["N5 1", "N5 2"],
+        ["N5 3", "N5 4"],
+        ["N5 5", "N5 6"],
+        ["N5 7", "N5 8"],
+        ["N5 9", "N5 10"],
+        ["N5 11", "N5 12"],
+        ["N5 13", "N5 14"],
+        ["N5 15", "N5 16"],
+        ["N5 17", "N5 18"],
+        ["N5 19", "N5 20"],
+        ["N5 21", "N5 22"],
+        ["N5 23", "N5 24"],
+        ["N5 25", "Back"],
       ])
         .resize()
         .oneTime(false)
     );
   });
 
-  bot.hears("N5 1", (ctx) => {
-    const message = n5Kotoba1.map(removePipes).join("\n");
-    ctx.reply(message);
-  });
+  const n5MessageIds = {
+    "N5 1": 9,
+    "N5 2": 102,
+    "N5 3": 103,
+    "N5 4": 104,
+    "N5 5": 105,
+    "N5 6": 106,
+    "N5 7": 107,
+    "N5 8": 108,
+    "N5 9": 109,
+    "N5 10": 110,
+    "N5 11": 111,
+    "N5 12": 112,
+    "N5 13": 113,
+    "N5 14": 114,
+    "N5 15": 115,
+    "N5 16": 116,
+    "N5 17": 117,
+    "N5 18": 118,
+    "N5 19": 119,
+    "N5 20": 120,
+    "N5 21": 121,
+    "N5 22": 122,
+    "N5 23": 123,
+    "N5 24": 124,
+    "N5 25": 125,
+  };
 
-  bot.hears("N5 1 Quiz", (ctx) => {
-    const randomN5Kotoba =
-      n5Kotoba1[Math.floor(Math.random() * n5Kotoba1.length)];
-    const escapedMessage = escapeMarkdownV2(randomN5Kotoba);
-    ctx.replyWithMarkdownV2(escapedMessage);
-  });
-
-  bot.hears("N5 2", (ctx) => {
-    const message = n5Kotoba2.map(removePipes).join("\n");
-    ctx.reply(message);
-  });
-
-  bot.hears("N5 2 Quiz", (ctx) => {
-    const randomN5Kotoba =
-      n5Kotoba2[Math.floor(Math.random() * n5Kotoba2.length)];
-    const escapedMessage = escapeMarkdownV2(randomN5Kotoba);
-    ctx.replyWithMarkdownV2(escapedMessage);
+  Object.keys(n5MessageIds).forEach((key) => {
+    bot.hears(key, async (ctx) => {
+      const messageId = n5MessageIds[key];
+      const channelId = -1002250750536; // Your channel's chat ID
+      try {
+        await ctx.telegram.copyMessage(ctx.chat.id, channelId, messageId);
+      } catch (error) {
+        console.error("Error forwarding message:", error);
+        await ctx.reply("Sorry, I could not forward the message.");
+      }
+    });
   });
   //kotoba
 
